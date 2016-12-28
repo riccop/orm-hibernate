4d1482ee1635e8d6bc3badfc5f9b8a1944208004
Ricardo Peixoto <Ricardo Peixoto>
2016-12-28 14:48:07 +0000

    User - Java class

---
+++ User.java
@@ -0,0 +1,21 @@
+public class User{
+	
+	int id;
+	String name;
+	String phone;
+	String address;
+	String dayOfBirth;
+	
+	public getId(){return id;}
+	public getName(){return name;}
+	public getPhone(){return phone;}
+	public getAddress(){return address;}
+	public getDayOfBirth(){return dayOfBirth;}
+	
+	public setId(int id){return this.id=id;}
+	public setName(){return this.name = name;}
+	public setPhone(){return this.phone = phone;}
+	public setAddress(){return this.address=address;}
+	public setDayOfBirth(){return this.dayOfBirth = dayOfBirth;}
+	
+}
